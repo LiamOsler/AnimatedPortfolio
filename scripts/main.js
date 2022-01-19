663ad3f5e9923c14a512ce8f7170756cb7f806d6
LiamOsler <liam.osler@dal.ca>
2022-01-18 23:46:31 -0400

    clean up main.js and add relevant comments

--- scripts/main.js
+++ scripts/main.js
@@ -3,49 +3,14 @@ function coordinateInverse(x1, y1, x2, y2){
     return Math.hypot(x2-x1, y2-y1)
 }
 
-//Generates a random color code in Hex:
+//Generates a random color code in Hex (hard fixed to grey right now)
 function randomColor(){
    return "E8E8E8";
 }
 
-console.log(randomColor());
-
 //Function that returns a random bit of either 1 or -1:
 function bitFlipper(){
    return directionRandomization = Math.round(Math.random()) * 2 - 1;
-}
-
-var playerRadius = 25;
-
-var startingPosition ={
-    "posX" : "",
-    "posY" : "",
-    "radius" : 25,
-    "color" : "#00FF00"
-};
-
-var endingPosition ={
-    "posX" : "",
-    "posY" : "",
-    "radius" : "",
-    "color" : "#FF0000"
-};
-
-function randomizeStartFinish(){
-    startingPosition.posX = Math.random()*maxX;
-    startingPosition.posY = Math.random()*maxY;
-    endingPosition.posX = Math.floor(Math.random()*maxX);
-    endingPosition.posY = Math.floor(Math.random()*maxY);
-    endingPosition.radius = startingPosition.radius + 10;
-}
-
-function drawStartFinish(){
-    drawCircle(startingPosition);
-    ctx.font = '12px sans-serif';
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillText("Start Here", 20, 20);
-
-    drawCircle(endingPosition);
 }
 
 var circles = [];
@@ -55,15 +20,11 @@ function randomizeCircleArray(){
    for(let i = 0; i < 100; i++){
        //Randomize the position and radius of the circles:
        var curRadius = 10 + Math.floor(Math.random() * radiusVariance);
-
        var curPosX = midX + (Math.floor(Math.random()*100))*bitFlipper();
        var curPosY = midY + (Math.floor(Math.random()*100))*bitFlipper();
-       
        var curVelX = ((Math.random() * velocityVariance))*bitFlipper();
        var curVelY = ((Math.random() * velocityVariance))*bitFlipper();
-
        var curColor = "#" + randomColor();
-
        //Create a circle object based on the random values:
        var currentCircle = circleFactory(curPosX, curPosY, curVelX , curVelY, curRadius, curColor);
       
@@ -72,13 +33,13 @@ function randomizeCircleArray(){
    }
 }
 
+//clears the contents of the circle array, if it need to be regenerated
 function clearCircleArray(){
     for(let i = 0; i < 100; i++){
         circles.pop(i);
     }
 }
 
-//Method for constructing circle objects:
 //posX: center x position, posY:  center y position, velX: the x velocity component, velY: the y velocity component, radius: the radius of the circle 
 function circleFactory(posX, posY, velX, velY, radius, color){
    var currentCircle = {
@@ -93,7 +54,6 @@ function circleFactory(posX, posY, velX, velY, radius, color){
    }
    return currentCircle;
 }
-
 
 //function for drawing circleObjects on the canvas:
 function drawCircle(circleObject){
@@ -119,6 +79,7 @@ function drawCircleArray(){
    }
 }
 
+//Move the circles around, but keeps them within the bounds of the page:
 function animateCircleArray(){
    for(let i = 0; i < circles.length; i++){
        circles[i].posX += circles[i].velX;
@@ -133,26 +94,17 @@ function animateCircleArray(){
    }
 }
 
+//checks if circles collide with the mouse:
 function circleCollisionDetection(){
    for(let i = 0; i < circles.length; i++){
         var distance = coordinateInverse(mouseX, mouseY, circles[i].posX, circles[i].posY);
-        //console.log(distance);
-
         if(distance< 50 + circles[i].radius){
             circles[i].collisionState = true;
         }
         else{
             circles[i].collisionState = false;
         }
-
     }
-}
-
-function drawCursor(){
-   ctx.fillStyle = "#FF0000";
-   ctx.beginPath();
-   ctx.arc(mouseX, mouseY, playerRadius, 0, 2 * Math.PI);
-   ctx.fill();
 }
 
 //Basic canvas setup:
@@ -170,10 +122,6 @@ c.addEventListener("mousemove", function(e) {
 });
 
 regenerate = false;
-
-
-
-
 var frameCount = 0;
 function frameCounter(){
    frameCount++;
@@ -190,23 +138,20 @@ function init(){
    midX = window.innerWidth/2;
    midY = maxY/2;
    //Set frame count to 0?
-
    randomizeCircleArray();
-   randomizeStartFinish();
-
    //Setting up some random stuff to draw:
    //These can be removed from init:
    ctx.lineWidth = 10;
    ctx.strokeStyle = "#FFFFFF"
 }
 
+//Animation function:
 var animationState = true;
 var gameState;
 function animate(){
     if(regenerate == true){
         clearCircleArray();
         randomizeCircleArray();
-        randomizeStartFinish();
         regenerate = false;
     }
 
@@ -215,12 +160,12 @@ function animate(){
         circleCollisionDetection();
     }   
 }
-
+//Main draw function:
 function draw(){
     drawCircleArray();
 }
 
-
+//Main runner for the canvas:
 function run() {
    frameCounter();
    // Clear the canvas:
@@ -230,8 +175,7 @@ function run() {
    setTimeout(run, 1000 / 60); // 60 fps   
 }
 
-
-
+//Canvas must be init to create circle array before run loop can be used:
 init();
 // Start the animation
 run();
